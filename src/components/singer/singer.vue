9f5fe1cf12da3348d94c342984caa02f6c41dbe1
wengbiancheng <1569702426@qq.com>
2018-08-22 22:58:16 +0800

    歌手页面：完成歌手列表数据的排序和重新包装

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -5,6 +5,10 @@
 <script type="text/ecmascript-6">
   import {getSingerList} from '../../api/singer';
   import {ERR_OK} from '../../api/config';
+  import Singer from 'common/js/singer';
+
+  const HOT_SINGER_LEN = 10;
+  const HOT_NAME = '热门';
 
   export default {
     data() {
@@ -19,9 +23,55 @@
       _getSingerList: function () {
         getSingerList().then((res) => {
           if (res.code === ERR_OK) {
-            console.log(res.data.list);
+            this.singers = this._normailzeSinger(res.data.list);
+            console.log(this.singers);
           }
         });
+      },
+      _normailzeSinger: function (list) {
+        let map = {
+          hot: {
+            title: HOT_NAME,
+            items: []
+          }
+        };
+        list.forEach((item, index) => {
+          if (index < HOT_SINGER_LEN) {
+            map.hot.items.push(new Singer({
+              name: item.Fsinger_name,
+              id: item.Fsinger_mid
+            }));
+          }
+
+          const key = item.Findex;
+          if (!map[key]) {
+            map[key] = {
+              title: key,
+              items: []
+            };
+          }
+          map[key].items.push(new Singer({
+            name: item.Fsinger_name,
+            id: item.Fsinger_mid
+          }));
+        });
+
+        // 为了得到有序的列表，我们需要处理map:a~Z
+        let ret = [];
+        let hot = [];
+        for (let key in map) {
+          let val = map[key];
+          if (val.title.match(/[a-zA-Z]/)) {
+            ret.push(val);
+          } else if (val.title === HOT_NAME) {
+            hot.push(val);
+          }
+        }
+        ret.sort((a, b) => {
+          return a.title.charCodeAt(0) - b.title.charCodeAt(0);
+        });
+
+        return hot.concat(ret);
       }
     }
   };
