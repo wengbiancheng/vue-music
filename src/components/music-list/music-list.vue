8fd902eb5332d2c13d77cfa23a1197a9bd44b816
wengbiancheng <1569702426@qq.com>
2018-08-29 00:03:02 +0800

    歌手详情页面：完成歌手详情顶部背景图片的播放控件的布局和样式，以及适配相应的上拉交互

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="music-list">
-    <div class="back">
+    <div class="back" @click="back">
       <i class="icon-back"></i>
     </div>
     <h1 class="title" v-html="title"></h1>
     <div class="bg-image" :style="bgStyle" ref="bgImage">
+      <div class="play-wrapper">
+        <div class="play" ref="playBtn" v-show="songs.length > 0">
+          <i class="icon-play"></i>
+          <span class="text">随机播放全部</span>
+        </div>
+      </div>
       <div class="filter" ref="filter"></div>
     </div>
     <div class="bg-layer" ref="layer"></div>
@@ -71,9 +77,11 @@
           zIndex = 10;
           this.$refs.bgImage.style.paddingTop = 0;
           this.$refs.bgImage.style.height = `${RESERVED_HEIGHT}px`;
+          this.$refs.playBtn.style.display = 'none';
         } else {
           this.$refs.bgImage.style.paddingTop = '70%';
           this.$refs.bgImage.style.height = 0;
+          this.$refs.playBtn.style.display = '';
         }
         this.$refs.bgImage.style[transform] = `scale(${scale})`;
         this.$refs.filter.style[backdrop] = `blur(${blur}px)`;
@@ -93,6 +101,9 @@
     methods: {
       scroll: function (pos) {
         this.scrollY = pos.y;
+      },
+      back: function () {
+        this.$router.back();
       }
     },
     components: {
@@ -142,6 +153,30 @@
       padding-top: 70%
       transform-origin: top
       background-size: cover
+      .play-wrapper
+        position: absolute
+        bottom: 20px
+        z-index: 50
+        width: 100%
+        .play
+          box-sizing: border-box
+          width: 135px
+          padding: 7px 0px
+          margin: 0px auto
+          border: 1px solid $color-theme
+          border-radius: 100px
+          text-align: center
+          font-size: 0
+          color: $color-theme
+          .icon-play
+            display: inline-block
+            vertical-align middle
+            margin-right: 6px
+            font-size: $font-size-medium-x
+          .text
+            display: inline-block
+            vertical-align: middle
+            font-size: $font-size-small
       .filter
         position: absolute
         top: 0
