7d59e83eb373baed4cfa6fb19ce561be350a460a
wengbiancheng <1569702426@qq.com>
2018-08-29 23:08:37 +0800

    播放器页面：完成播放器页面的基本布局和基本样式

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -1,7 +1,60 @@
 <template>
   <div class="player" v-show="playlist.length > 0">
-    <div class="normal-player" v-show="fullScreen">播放器</div>
-    <div class="mini-player" v-show="!fullScreen"></div>
+    <div class="normal-player" v-show="fullScreen">
+      <div class="background">
+        <img width="100%" height="100%">
+      </div>
+      <div class="top">
+        <div class="back">
+          <i class="icon-back"></i>
+        </div>
+        <h1 class="title"></h1>
+        <h2 class="subtitle"></h2>
+      </div>
+      <div class="middle">
+        <div class="middle-l">
+          <div class="cd-wrapper">
+            <div class="cd">
+              <img class="image">
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="bottom">
+        <div class="operators">
+          <div class="icon i-left">
+            <i class="icon-sequence"></i>
+          </div>
+          <div class="icon i-left">
+            <i class="icon-prev"></i>
+          </div>
+          <div class="icon i-center">
+            <i class="icon-play"></i>
+          </div>
+          <div class="icon i-right">
+            <i class="icon-next"></i>
+          </div>
+          <div class="icon i-right">
+            <i class="icon icon-not-favorite"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="mini-player" v-show="!fullScreen">
+      <div class="icon">
+        <div class="imgWrapper">
+          <img width="40" height="40">
+        </div>
+      </div>
+      <div class="text">
+        <h2 class="name"></h2>
+        <p class="desc"></p>
+      </div>
+      <div class="control"></div>
+      <div class="control">
+        <i class="icon-playlist"></i>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -22,6 +75,7 @@
 
 <style lang="stylus" rel="stylesheet/stylus">
   @import "../../common/stylus/variable.styl";
+  @import "../../common/stylus/mixin.styl";
   .player
     .normal-player
       position: fixed
@@ -31,13 +85,137 @@
       bottom: 0
       z-index: 150
       background: $color-background
+      .background
+        position: absolute
+        top: 0px
+        left: 0px
+        width: 100%
+        height: 100%
+        z-index: -1
+        opacity: 0.6
+        filter: blur(20px)
+      .top
+        position: relative
+        margin-bottom: 25px
+        .back
+          position: absolute
+          top: 0px
+          left: 6px
+          z-index: 50
+          .icon-back
+            display: block
+            padding: 9px
+            font-size: $font-size-large-x
+            color: $color-theme
+            transform: rotate(-90deg)
+        .title
+          width: 70%
+          margin: 0 auto
+          line-height: 40px
+          text-align: center
+          font-size: $font-size-large
+          color: $color-text
+          no-wrap()
+      .middle
+        position: fixed
+        width: 100%
+        top: 80px
+        bottom: 170px
+        white-space: nowrap
+        font-size: 0
+        .middle-l
+          display: inline-block
+          vertical-align: top
+          position: relative
+          width: 100%
+          height: 0px
+          padding-top: 80%
+          .cd-wrapper
+            position: absolute
+            left: 10%
+            top: 0px
+            width: 80%
+            box-sizing: border-box
+            height: 100%
+            .cd
+              width: 100%
+              height: 100%
+              border-radius: 50%
+              .img
+                position: absolute
+                top: 0px
+                left: 0px
+                width: 100%
+                height: 100%
+                box-sizing: border-box
+                border-radius: 50%
+                border: 10px solid rgba(255, 255, 255, 0.1)
+      .bottom
+        position: absolute
+        bottom: 50px
+        width: 100%
+        .operators
+          display: flex
+          align-items: center
+          .icon
+            flex: 1
+            color: $color-theme
+            i
+              font-size: 30px
+
+          .i-left
+            text-align: right
+          .i-center
+            padding: 0px 20px
+            text-align: center
+            i
+              font-size: 40px
+          .i-right
+            text-align: left
+          .icon-not-favorite
+            color: $color-sub-theme
     .mini-player
       display: flex
       align-items: center
       position: fixed
       left: 0
       bottom: 0
+      z-index: 180
       width: 100%
       height: 60px
       background: $color-highlight-background
+      .icon
+        flex: 0 0 40px
+        width: 40px
+        height: 40px
+        padding: 0px 10px 0px 20px
+        .imgWrapper
+          width: 100%
+          height: 100%
+          img
+            border-radius: 50%
+      .text
+        display: flex
+        flex-direction: column
+        justify-content: center
+        flex: 1
+        line-height: 20px
+        overflow: hidden
+        font-size: 0px
+        .name
+          margin-bottom: 2px
+          no-wrap()
+          font-size: $font-size-medium
+          color: $color-text
+        .desc
+          no-wrap()
+          font-size: $font-size-small
+          color: $color-text-d
+      .control
+        flex: 0 0 30px
+        width: 30px
+        padding: 0px 10px
+        .icon-playlist
+          font-size: 30px
+          color: $color-theme-d
 </style>
